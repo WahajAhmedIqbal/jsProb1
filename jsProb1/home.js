a3842ca030e9f44ec63a483de18f94373f409fa8
wahaj ahmed iqbal <wahaj465@gmail.com>
2020-05-31 12:25:30 +0500

    first prob complete

---
+++ jsProb1/home.js
@@ -0,0 +1,9 @@
+//  Problem # 1 ///////////////////////////////////////////
+////// find sum of the odd numbers below 1000
+var sum = 0;
+for ( i = 1; i <= 1000; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+        sum += i;    
+    }    
+}
+console.log(sum)
